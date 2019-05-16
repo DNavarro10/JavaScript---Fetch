6ae929db887e280e457958af7d57e1854c87569d
DNavarro10 <luis_d_100@hotmail.com>
2019-05-15 23:08:04 -0600

    peticion rest

--- JS/fetch.js
+++ JS/fetch.js
@@ -1,2 +1,11 @@
 'use strict'
 
+// fetch y peticiones/ Apis rest
+let usuario = [];
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(data => data.json())
+.then( user =>{
+    usuario =  user[0].name;
+    console.log(usuario)
+});
