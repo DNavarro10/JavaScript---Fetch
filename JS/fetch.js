e714c2af29082cbfe2adcb2a49338e26e221ce2f
DNavarro10 <luis_d_100@hotmail.com>
2019-05-18 22:18:54 -0600

    funcionando

--- JS/fetch.js
+++ JS/fetch.js
@@ -1,22 +1,27 @@
 'use strict'
 
 // fetch y peticiones/ Apis rest
-let div_user = document.querySelector("#usuarios");
-let usuarios = [];
 
-fetch('https://reqres.in/api/users')
-.then(data => data.json())
-.then(user =>{
-    usuarios =  user.data;
-    console.log(usuarios);
+    let div_user = document.querySelector("#usuarios");
+    let usuarios = [];
 
-    usuarios.map ((user, i) => {
-        let nombre = document.createElement('h4');
+    fetch('https://reqres.in/api/users')
+        .then(data => data.json())
+        .then(user => {
+            usuarios = user.data;
+            console.log(usuarios);
+
+            usuarios.map((user, i) => {
+                let nombre = document.createElement('h5');
 
 
-        nombre.innerHTML = i + user.first_name + " " + user.last_name;
-        console.log(div_user);
-        div_user.append(nombre);
-        console.log(div_user);
-    });
-});
+                nombre.innerHTML = ` ${i} - ${user.first_name} ${user.last_name}`;
+
+                div_user.appendChild(nombre);
+
+                //no mostrar load
+                document.querySelector(".load").style.display = 'none';
+            });
+        });
+
+
